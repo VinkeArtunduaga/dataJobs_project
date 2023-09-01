b9263c690fca3e970ccfdb9ee17323983f78d06c
Kevin Santiago Artunduaga Vivas <kevin.artunduga@uao.edu.co>
2023-09-01 17:30:01 -0500

    Segundo commit

--- dataJobs_script.py
+++ dataJobs_script.py
@@ -1,6 +1,7 @@
 import psycopg2
 import csv
 import json
+import pandas as pd
 
 #Carga de la configuracion de los datos de la database
 with open('db_config.json') as config_file:
@@ -18,6 +19,8 @@ try:
 
     cursor = conn.cursor()
 
+    #Creacion de la tabla dataJobs_glassdoor
+    
     create_table = """
         CREATE TABLE IF NOT EXISTS dataJobs_glassdoor (
             id SERIAL PRIMARY KEY,
@@ -190,6 +193,7 @@ try:
     conn.commit()
     print("Tabla creada")
 
+    # Insercion de las columnas a postgresql
     copy_query = """
         COPY dataJobs_glassdoor(
             benefits_benefitRatingDecimal, benefits_comments, benefits_highlights, 
@@ -306,14 +310,14 @@ try:
         'salary_lastSalaryDate', 'wwfu'
     ]
 
-    #Recorro columna por columna para eliminarlas
+    # Recorro columna por columna para eliminarlas
     for columna in columnas_eliminadas:
         drop_column_query = f"ALTER TABLE dataJobs_glassdoor DROP COLUMN IF EXISTS {columna};"
         cursor.execute(drop_column_query)
         print(f"Columna {columna} eliminada")
     conn.commit()
 
-    # Reemplazar valores nulos
+    # Reemplazar valores nulos en las columnas mencionadas
     update_query = """
         UPDATE dataJobs_glassdoor
         SET
@@ -327,6 +331,178 @@ try:
     cursor.execute(update_query)
     conn.commit()
     print("Valores nulos reemplazados")
+
+    # Consulta para obtener valores únicos de la columna header_jobTitle
+    unique_job_titles_query = """
+        SELECT DISTINCT header_jobTitle
+        FROM dataJobs_glassdoor
+    """
+    cursor.execute(unique_job_titles_query)
+    unique_job_titles = cursor.fetchall()
+    job_titles_list = [title[0] for title in unique_job_titles]
+    job_titles_str = ', '.join(job_titles_list)
+    print("Valores únicos de header_jobTitle:")
+    print(job_titles_str)
+
+    # Creacion de la columna jobTitle_normalized
+    Job_normalized_column = """
+        ALTER TABLE IF EXISTS dataJobs_glassdoor
+        ADD COLUMN IF NOT EXISTS jobTitle_normalized VARCHAR;
+    """
+    cursor.execute(Job_normalized_column)
+    conn.commit()
+    print("Columna jobTitle_normalized agregada")
+
+    # Consulta para obtener los valores únicos de la columna 'jobTitle_normalized'
+    query_jobTitle = "SELECT DISTINCT jobTitle_normalized FROM dataJobs_glassdoor"
+    cursor.execute(query_jobTitle)
+
+    # Obtener los resultados y imprimir los valores únicos
+    unique_titles = cursor.fetchall()
+    for title in unique_titles:
+        print(title[0])  
+
+    # Actualizar la columna jobTitle_normalized basada en los valores de header_jobTitle
+    update_jobtitle = """
+        UPDATE dataJobs_glassdoor
+        SET jobTitle_normalized = CASE
+            WHEN header_jobTitle ILIKE '%Business Analyst%' THEN 'Business'
+            WHEN header_jobTitle ILIKE '%Business%' THEN 'Business'
+            WHEN header_jobTitle ILIKE '%Product Owner%' THEN 'Business' 
+            WHEN header_jobTitle ILIKE '%Schadebeheerder%' THEN 'Business' 
+            WHEN header_jobTitle ILIKE '%Marketing Specialist%' THEN 'Business' 
+            WHEN header_jobTitle ILIKE '%Product%' THEN 'Business' 
+            WHEN header_jobTitle ILIKE '%Project Buyer%' THEN 'Business' 
+            WHEN header_jobTitle ILIKE '%Accountancy%' THEN 'Business' 
+            WHEN header_jobTitle ILIKE '%Accountant%' THEN 'Business' 
+            WHEN header_jobTitle ILIKE '%DOSSIERBEHEERDER%' THEN 'Business' 
+            WHEN header_jobTitle ILIKE '%Dossierbeheerder%' THEN 'Business' 
+            WHEN header_jobTitle ILIKE '%Contractbeheerder%' THEN 'Business'
+            WHEN header_jobTitle ILIKE '%Løsningsarkitekter%' THEN 'Business' 
+            WHEN header_jobTitle ILIKE '%Klantenbeheerder%' THEN 'Business' 
+            WHEN header_jobTitle ILIKE '%Operationeel Beheerder%' THEN 'Business' 
+            WHEN header_jobTitle ILIKE '%Pöyry%' THEN 'Business' 
+            WHEN header_jobTitle ILIKE '%Project Administration%' THEN 'Business' 
+            WHEN header_jobTitle ILIKE '%Relatiebeheerder%' THEN 'Business'
+            WHEN header_jobTitle ILIKE '%Materials Science%' THEN 'Healthcare/Science'
+            WHEN header_jobTitle ILIKE '%Clinical Research%' THEN 'Healthcare/Science' 
+            WHEN header_jobTitle ILIKE '%Scientist%' THEN 'Healthcare/Science'
+            WHEN header_jobTitle ILIKE '%Computational Biologist%' THEN 'Healthcare/Science' 
+            WHEN header_jobTitle ILIKE '%Medical%' THEN 'Healthcare/Science'
+            WHEN header_jobTitle ILIKE '%Sr Lab%' THEN 'Healthcare/Science' 
+            WHEN header_jobTitle ILIKE '%Molecular%' THEN 'Healthcare/Science'
+            WHEN header_jobTitle ILIKE '%Global HR%' THEN 'Human Resources' 
+            WHEN header_jobTitle ILIKE '%Payrollbeheerder%' THEN 'Human Resources'
+            WHEN header_jobTitle ILIKE '%Analyst%' THEN 'Data Science/Analytics' 
+            WHEN header_jobTitle ILIKE '%Database Administrator%' THEN 'Data Science/Analytics'  
+            WHEN header_jobTitle ILIKE '%Datenbank%' THEN 'Data Science/Analytics'
+            WHEN header_jobTitle ILIKE '%Databasebeheerder%' THEN 'Data Science/Analytics' 
+            WHEN header_jobTitle ILIKE '%Deep Learning%' THEN 'Data Science/Analytics' 
+            WHEN header_jobTitle ILIKE '%Deep-learning%' THEN 'Data Science/Analytics' 
+            WHEN header_jobTitle ILIKE '%Data%' THEN 'Data Science/Analytics'
+            WHEN header_jobTitle ILIKE '%Server DBA%' THEN 'Data Science/Analytics' 
+            WHEN header_jobTitle ILIKE '%Données%' THEN 'Data Science/Analytics'
+            WHEN header_jobTitle ILIKE '%DBA%' THEN 'Data Science/Analytics'
+            WHEN header_jobTitle ILIKE '%INGÉNIEUR%' THEN 'Engineering' 
+            WHEN header_jobTitle ILIKE '%Eng%' THEN 'Engineering'
+            WHEN header_jobTitle ILIKE '%Engr%' THEN 'Engineering' 
+            WHEN header_jobTitle ILIKE '%ingenieur%' THEN 'Engineering' 
+            WHEN header_jobTitle ILIKE '%Projectbeheerder%' THEN 'Engineering' 
+            WHEN header_jobTitle ILIKE '%Wagenparkbeheerder%' THEN 'Engineering'
+            WHEN header_jobTitle ILIKE '%Project Administrator%' THEN 'Management'
+            WHEN header_jobTitle ILIKE '%Projects Financial%' THEN 'Management' 
+            WHEN header_jobTitle ILIKE '%Project Coordinator%' THEN 'Management'
+            WHEN header_jobTitle ILIKE '%Project Management%' THEN 'Management' 
+            WHEN header_jobTitle ILIKE '%Project Planning%' THEN 'Management'
+            WHEN header_jobTitle ILIKE '%Functioneel Beheerder%' THEN 'Management' 
+            WHEN header_jobTitle ILIKE '%Program Coordinator%' THEN 'Management' 
+            WHEN header_jobTitle ILIKE '%Project Specialist%' THEN 'Management' 
+            WHEN header_jobTitle ILIKE '%Project Assistant%' THEN 'Management'
+            WHEN header_jobTitle ILIKE '%Project Support%' THEN 'Management' 
+            WHEN header_jobTitle ILIKE '%Delivery Project%' THEN 'Management' 
+            WHEN header_jobTitle ILIKE '%Program Management%' THEN 'Management' 
+            WHEN header_jobTitle ILIKE '%Project Co-ordinator%' THEN 'Management' 
+            WHEN header_jobTitle ILIKE '%Project Officer%' THEN 'Management'
+            WHEN header_jobTitle ILIKE '%MANAGEMENT%' THEN 'Management' 
+            WHEN header_jobTitle ILIKE '%Project Planner%' THEN 'Management'
+            WHEN header_jobTitle ILIKE '%Project Controller%' THEN 'Management' 
+            WHEN header_jobTitle ILIKE '%Project Office%' THEN 'Management' 
+            WHEN header_jobTitle ILIKE '%Project Admin%' THEN 'Management' 
+            WHEN header_jobTitle ILIKE '%Projectvoorbereider%' THEN 'Management' 
+            WHEN header_jobTitle ILIKE '%Projectleider%' THEN 'Management'
+            WHEN header_jobTitle ILIKE '%Applicatiebeheerder%' THEN 'Technology/IT' 
+            WHEN header_jobTitle ILIKE '%Systeembeheerder%' THEN 'Technology/IT' 
+            WHEN header_jobTitle ILIKE '%Programmer%' THEN 'Technology/IT' 
+            WHEN header_jobTitle ILIKE '%SOFTWARE%' THEN 'Technology/IT'
+            WHEN header_jobTitle ILIKE '%Computer Scientist%' THEN 'Technology/IT' 
+            WHEN header_jobTitle ILIKE '%Software Developer%' THEN 'Technology/IT' 
+            WHEN header_jobTitle ILIKE '%Machine Learning%' THEN 'Technology/IT' 
+            WHEN header_jobTitle ILIKE '%Werkplekbeheerder%' THEN 'Technology/IT' 
+            WHEN header_jobTitle ILIKE '%Netwerkbeheerder%' THEN 'Technology/IT' 
+            WHEN header_jobTitle ILIKE '%MJUKVARUUTVECKLARE%' THEN 'Technology/IT' 
+            WHEN header_jobTitle ILIKE '%Technology Consulting%' THEN 'Technology/IT' 
+            WHEN header_jobTitle ILIKE '%Technischer Berater%' THEN 'Technology/IT' 
+            WHEN header_jobTitle ILIKE '%Beheerder IT%' THEN 'Technology/IT' 
+            WHEN header_jobTitle ILIKE '%Développement Java%' THEN 'Technology/IT' 
+            WHEN header_jobTitle ILIKE '%Cloud applicatie%' THEN 'Technology/IT' 
+            WHEN header_jobTitle ILIKE '%ICT beheerder%' THEN 'Technology/IT' 
+            WHEN header_jobTitle ILIKE '%Linux Beheerder%' THEN 'Technology/IT' 
+            WHEN header_jobTitle ILIKE '%Project Executive%' THEN 'Technology/IT' 
+            WHEN header_jobTitle ILIKE '%Angular%' THEN 'Technology/IT' 
+            WHEN header_jobTitle ILIKE '%Informatie analist%' THEN 'Technology/IT'
+            WHEN header_jobTitle ILIKE '%IT%' THEN 'Technology/IT' 
+            WHEN header_jobTitle ILIKE '%Technisch Beheerder%' THEN 'Technology/IT' 
+            WHEN header_jobTitle ILIKE '%Technisch%' THEN 'Technology/IT' 
+            WHEN header_jobTitle ILIKE '%Technieker%' THEN 'Technology/IT'
+            WHEN header_jobTitle ILIKE '%Burgerzaken%' THEN 'Government' 
+            WHEN header_jobTitle ILIKE '%Ketenbeheerder%' THEN 'Logistics/Supply Chain' 
+            WHEN header_jobTitle ILIKE '%stockbeheerder%' THEN 'Logistics/Supply Chain' 
+            WHEN header_jobTitle ILIKE '%Magazijnbeheerder%' THEN 'Logistics/Supply Chain' 
+            WHEN header_jobTitle ILIKE '%Orderbeheerder%' THEN 'Logistics/Supply Chain' 
+            WHEN header_jobTitle ILIKE '%Magazijn%' THEN 'Logistics/Supply Chain'  
+            WHEN header_jobTitle ILIKE '%Project Material%' THEN 'Logistics/Supply Chain'
+            WHEN header_jobTitle ILIKE '%Research%' THEN 'Research' 
+            WHEN header_jobTitle ILIKE '%Building%' THEN 'Architecture/Construction' 
+            WHEN header_jobTitle ILIKE '%BIM Modeller%' THEN 'Architecture/Construction' 
+            WHEN header_jobTitle ILIKE '%Modelleur%' THEN 'Architecture/Construction' 
+            WHEN header_jobTitle ILIKE '%Construction%' THEN 'Architecture/Construction'
+            WHEN header_jobTitle ILIKE '%到校學前康復服務%' THEN 'Education' 
+            WHEN header_jobTitle ILIKE '%School%' THEN 'Education' 
+            WHEN header_jobTitle ILIKE '%Cateringbeheerder%' THEN 'Food Industry' 
+            WHEN header_jobTitle ILIKE '%Manufacturing%' THEN 'Manufacturing'  
+            WHEN header_jobTitle ILIKE '%Debiteurenbeheerder%' THEN 'Manufacturing'
+            WHEN header_jobTitle ILIKE '%Transportation%' THEN 'Transportation' 
+            WHEN header_jobTitle ILIKE '%Fleet Programs%' THEN 'Transportation' 
+            WHEN header_jobTitle ILIKE '%Polisbeheerder%' THEN 'Transportation' 
+            WHEN header_jobTitle ILIKE '%Hotelbeheerder%' THEN 'Tourism/Hospitality'  
+            WHEN header_jobTitle ILIKE '%Commercieel bediende%' THEN 'Retail' 
+            WHEN header_jobTitle ILIKE '%Modelmaker%' THEN 'Design'
+            ELSE jobTitle_normalized
+        END;
+    """
+    cursor.execute(update_jobtitle)
+    conn.commit()
+    print("Valores actualizados en jobTitle_normalized")
+
+    # Cambio de vacios en jobTilte_normalized por otros
+    update_empty = """
+        UPDATE dataJobs_glassdoor
+        SET jobTitle_normalized = 'Other'
+        WHERE jobTitle_normalized = '';
+    """
+    cursor.execute(update_empty)
+    conn.commit()
+    print("Campos vacios cambiados")
+
+    # Cambio de nulos en overview type
+    update_null_overview_type= """
+        UPDATE dataJobs_glassdoor
+        SET overview_type = 'Unknown'
+        WHERE overview_type IS NULL;
+    """
+    cursor.execute(update_null_overview_type)
+    conn.commit()
+    print("Valores nulos en overview_type actualizados")
 
 except Exception as ex:
     print("Error:", ex)
--- df_tocsv.py
+++ df_tocsv.py
@@ -1,8 +1,141 @@
 import pandas as pd
+import psycopg2
 
-df = pd.read_csv("C:/Users/kevin/ETL/Postgres/kaggle/dataJobs_glassdoor.csv", delimiter=',')
+df = pd.read_csv("C:/Users/kevin/ETL/Postgres/kaggle/dataJobs_project/dataJobs_all.csv", delimiter=',')
 
-output_filename = 'dataJobs_all.csv'
-df.to_csv(output_filename, index=False)
+# output_filename = 'dataJobs_all.csv'
+# df.to_csv(output_filename, index=False)
 
-print(f"CSV guardado como '{output_filename}' exitosamente.")
+# print(f"CSV guardado como '{output_filename}' exitosamente.")
+
+category_mapping = {
+
+    "Technology/IT": [
+        "Java Team Lead", "Software Engineer", "Developer", "Technical Support Engineer",
+        "AI Engineer", "Cloud Engineer", "Data Analyst", "Security Data Scientist",
+        "Data Engineer", "Data Center Technician (DCO)",
+        "Software Developer within Automotive", "Cloud Engineer/DevOps Engineer",
+        "System Analyst/Backend Developer", "Junior Big Data Engineer",
+        "Sr Software Engineer, Ad tech", "Software Engineer – Networks"
+    ],
+    "Management": ["Manager", "Director", "Lead"],
+    "Data Science/Analytics": [
+        "Data Scientist", "Statistician", "Market Research Analyst",
+        "Big Data Analyst (m/w)"
+    ],
+    "Business": [
+        "Sales Manager", "Account Manager", "Business Development",
+        "Marketing Manager", "Product Manager", "Digital Marketing",
+        "Finance Analyst", "Financial Manager", "Consultant", "Advisor",
+        "Associate Practice Engagement Manager",
+        "Proactive Monitoring Engineer-Commerce Cloud",
+        "Agile Business Analyst", "Associate"
+    ],
+    "Healthcare/Science": [
+        "Doctor", "Nurse", "Medical Scientist", "Biomedical Scientist",
+        "Clinical Research Associate", "Genetic Counselor",
+        "Senior Research Scientist – Antibody reagents: in vivo biology",
+        "Public Health Specialist Manager",
+        "Clinical Scientist in Pharma Research and Early Development (m/f/d)"
+    ],
+    "Engineering": [
+        "Engineer", "Project Engineer", "Geotechnical Engineer",
+        "Controls Engineer", "Energy Engineer", "Aerospace Engineer",
+        "Structural and Foundation Engineer",
+        "BMS Controls Engineer", "Energy Engineer",
+        "Automotive Data Analyst", "Construction Engineer",
+        "Structural and Foundation Engineer",
+        "Associate, Big Data Analyst (m/w)", "Sr.Equipment Engineer"
+    ],
+    "Education": [
+        "Assistant Professor", "Lecturer", "Educational Consultant",
+        "Assistant Professor - Computer Sciences",
+        "Territory Manager (Ponta Grossa/PR)"
+    ],
+    "Logistics/Supply Chain": ["Supply Chain Analyst", "Logistics Manager"],
+    "Quality Assurance": ["Quality Assurance Analyst"],
+    "Telecommunications": ["Telecommunications Engineer"],
+    "Entertainment": [
+        "Entertainment Analyst", "Media Analyst", "Sports Analyst",
+        "Music Producer", "Music Instructor"
+    ],
+    "Environment": ["Environmental Engineer", "Climate Change Analyst"],
+    "Legal": ["Legal Analyst", "Legal Consultant"],
+    "Design": ["Designer", "Art Director", "Interior Designer"],
+    "Social Services": ["Social Worker", "Nonprofit Coordinator"],
+    "Energy": ["Energy Analyst", "Renewable Energy Engineer"],
+    "Government": ["Government Analyst"],
+    "Retail": ["Retail Manager"],
+    "Research": [
+        "Research Scientist", "Research Analyst",
+        "Research Scientist - Computer Vision",
+        "Senior Research Scientist", "Assistant Professor - Computer Sciences"
+    ],
+    "Human Resources": ["HR Manager", "Recruiter"],
+    "Architecture/Construction": ["Architect", "Construction Engineer"],
+    "Customer Service": ["Customer Service Representative"],
+    "Manufacturing": [
+        "Manufacturing Engineer", "Quality Control Analyst"
+    ],
+    "Food Industry": ["Executive Chef", "Food Quality Analyst"],
+    "Tourism/Hospitality": [
+        "Tourism Coordinator", "Hospitality Manager", "Travel Consultant"
+    ],
+    "Arts/Creative": [
+        "Creative Writer", "Musician", "Visual Artist",
+        "Fashion Designer", "Textile Analyst", "Art Director"
+    ],
+    "Language/Linguistics": ["Linguist", "Language Teacher"],
+    "Psychology": ["Psychologist", "Behavioral Analyst"],
+    "History/Heritage": [
+        "Historian", "Historical Preservation Specialist",
+        "Archaeologist", "Historical Researcher"
+    ],
+    "Transportation": [
+        "Transportation Planner", "Aviation Engineer", "Logistics Analyst",
+        "Roving Project Manager", "Project Manager - Mechanical + Electrical",
+        "Territory Manager (Outdoor Sales Consultant)"
+    ]
+    # ... (otras categorías)
+}
+
+# Crear una nueva columna 'jobTitle_normalized'
+df['jobTitle_normalized'] = ""
+
+# Realizar las transformaciones y mapeo a categorías
+for cat, titles in category_mapping.items():
+    matching_rows = df['header.jobTitle'].str.contains('|'.join(titles), case=False)
+    df.loc[matching_rows, 'jobTitle_normalized'] = cat
+    print(f"Registrados {matching_rows.sum()} títulos en la categoría {cat}")
+
+# Asignar la categoría "Other" a los títulos no mapeados
+other_rows = df['jobTitle_normalized'].isnull()
+df.loc[other_rows, 'jobTitle_normalized'] = "Other"
+print(f"Registrados {other_rows.sum()} títulos en la categoría Other")
+
+# Conexión a la base de datos PostgreSQL
+conn = psycopg2.connect(
+    database = "ETL",
+    user = "postgres",
+    password = "colamer13",
+    host = "localhost"
+)
+
+# Crear un cursor para ejecutar consultas
+cursor = conn.cursor()
+
+# Actualizar la columna 'header_jobtitle' en la base de datos
+for index, row in df.iterrows():
+    header_jobtitle = row['jobTitle_normalized']
+    update_query = f"""
+        UPDATE dataJobs_glassdoor
+        SET jobTitle_normalized = %s
+        WHERE id = %s
+    """
+    # Supongo que tu base de datos tiene una columna 'id'
+    cursor.execute(update_query, (header_jobtitle, index + 1))
+    print(f"Actualizada fila {index + 1} en la base de datos")
+
+conn.commit()
+cursor.close()
+conn.close()
